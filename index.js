bd9ed7b5941f09e913b1c1a853215e9e04f929c2
KubaJarmolowicz <kuba.jarmolowicz92@gmail.com>
2021-01-29 21:27:23 +0000

    Fighting with translate function

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 "use strict";
 
-console.log("test");
+const monthList = document.querySelector(".habit__months-list");
+const months = Array.from(document.querySelectorAll(".habit__month"));
+
+const startingListPosition = months[0].getBoundingClientRect().x;
+
+monthList.addEventListener("click", () => {
+	const currentListPosition =
+		startingListPosition - months[0].getBoundingClientRect().x;
+
+	const currentViewWidth = document
+		.querySelector(".habit__header")
+		.getBoundingClientRect().width;
+
+	const currentGap =
+		months[1].getBoundingClientRect().x -
+		(months[0].getBoundingClientRect().x +
+			months[0].getBoundingClientRect().width);
+
+	console.log(currentGap);
+
+	months.forEach(month => {
+		month.style.transform = `translateX(-${
+			currentListPosition + currentViewWidth - currentGap
+		}px)`;
+	});
+});
